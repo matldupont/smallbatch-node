872b054d6ecf8cf73fe3d4b9ab2ee24926b3e5ea
Mat Dupont <monolith.x@gmail.com>
2016-03-14 21:05:53 -0400

    inital commit

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -3,7 +3,7 @@
 // Init the application configuration module for AngularJS application
 var ApplicationConfiguration = (function () {
   // Init module configuration options
-  var applicationModuleName = 'mean';
+  var applicationModuleName = 'smallbatch-node';
   var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ngMessages', 'ui.router', 'ui.bootstrap', 'angularFileUpload'];
 
   // Add a new vertical module
