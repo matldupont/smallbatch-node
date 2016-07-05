f62c26a7d470846197275c56c9b52938d396bc62
Mat Dupont <monolith.x@gmail.com>
2016-07-04 20:56:36 -0400

    Enabled meals only

--- modules/meals/client/controllers/list-meals.client.controller.js
+++ modules/meals/client/controllers/list-meals.client.controller.js
@@ -9,6 +9,7 @@
 
   function MealsListController($scope, MealsService, CoursesService) {
     $scope.meals = MealsService.query(function(data) {
+      console.log(data);
       $scope.courses = CoursesService.query(function(data) {
         angular.forEach(data, function(course) {
           $scope.courseOrdinals[course._id] = course.ordinal;
